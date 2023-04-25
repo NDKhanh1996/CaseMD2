ffa0da33549244e722d4a01ef82382b4bf139bfe
Nguyễn Duy Khánh <khanh@MacBook-Air-cua-Nguyen.local>
2023-04-25 16:00:42 +0700

    add

--- exe.ts
+++ exe.ts
@@ -3,21 +3,14 @@ import {ClientManager} from "./src/ClientManager/ClientManager";
 import {Admin} from "./src/UserManager/User/EndUsers/Admin";
 
 ClientManager.buyClient(9)
-UserManager.createAccount('a', 1, 10000)
-UserManager.createAccount('b', 1, 15000)
-UserManager.createAccount('c', 1, 17000)
-UserManager.createAccount('d', 1, 16000)
-UserManager.createAccount('e', 1, 18000)
-//
-//
+
 // console.table(UserManager.getList())
 // console.table(ClientManager.getList(), ['clientName', 'onUsedBy'])
-
 
 export function start() {
     const readlineSync = require('readline-sync');
     const options = ['admin', 'user'];
-    let index = +readlineSync.keyInSelect(options, 'Choose your role: ') + 1;
+    let index = readlineSync.keyInSelect(options, 'Choose your role: ') + 1;
 
     switch (index) {
         case 1:
@@ -26,7 +19,7 @@ export function start() {
                 hideEchoBack: true
             });
             if (username === 'admin' && password === '1') {
-                Admin.logged()
+                Admin.option()
             }
             break;
         case 2:
--- src/UserManager/User/EndUsers/Admin.ts
+++ src/UserManager/User/EndUsers/Admin.ts
@@ -3,10 +3,9 @@ import {ClientManager} from "../../../ClientManager/ClientManager";
 import {start} from "../../../../exe";
 
 export class Admin {
-    static logged() {
+    static option() {
         const readlineSync = require('readline-sync');
-        const method = ['getUserList', 'getClientList', 'createAccount', 'getMoneyByName', 'setMoneyByName', 'getPasswordByName', 'setPasswordByName']
-
+        const method = ['getUserList', 'getClientList', 'createAccount', 'getMoneyByName', 'setMoneyByName', 'getPasswordByName', 'setPasswordByName'];
         const index = readlineSync.keyInSelect(method, 'choose what to do: ', {cancel: 'RETURN'}) + 1;
         switch (index) {
             case 0:
@@ -14,32 +13,46 @@ export class Admin {
                 break;
             case 1:
                 console.table(UserManager.getList());
-                Admin.logged();
+                Admin.option();
                 break;
             case 2:
                 console.table(ClientManager.getList());
-                Admin.logged();
+                Admin.option();
                 break;
-            case 3:
-                UserManager.createAccount('name', 1, 1);
-                Admin.logged();
+            case 3: {
+                const username = readlineSync.question('Enter username: ');
+                const password = readlineSync.question('Enter password: ');
+                const money = readlineSync.question('Enter money: ');
+                UserManager.createAccount(username, password, money);
+                Admin.option();
                 break;
-            case 4:
-                UserManager.getMoneyByName('a');
-                Admin.logged();
+            }
+            case 4: {
+                const username = readlineSync.question('Enter username');
+                UserManager.getMoneyByName(username);
+                Admin.option();
                 break;
-            case 5:
-                UserManager.setMoneyByName('a', 1);
-                Admin.logged();
+            }
+            case 5: {
+                const username = readlineSync.question('Enter username');
+                const money = readlineSync.question('Enter newMoney');
+                UserManager.setMoneyByName(username, money);
+                Admin.option();
                 break;
-            case 6:
-                UserManager.getPasswordByName('a');
-                Admin.logged();
+            }
+            case 6: {
+                const username = readlineSync.question('Enter username');
+                UserManager.getPasswordByName(username);
+                Admin.option();
                 break;
-            case 7:
-                UserManager.setPasswordByName('a', 1);
-                Admin.logged();
+            }
+            case 7: {
+                const username = readlineSync.question('Enter username');
+                const password = readlineSync.question('Enter password: ');
+                UserManager.setPasswordByName(username, password);
+                Admin.option();
                 break;
+            }
         }
     }
 }
